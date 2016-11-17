d6b7c2d74920d6a4c15e4f8cc98fecd53a9489df
Patrick Zawadzki <zawadzkip@msoe.edu>
2016-11-16 23:21:29 -0600

    completed workshop code.

--- MSOEWorkshop/MSOEWorkshop/MSOEWorkshopPage.xaml.cs
+++ MSOEWorkshop/MSOEWorkshop/MSOEWorkshopPage.xaml.cs
@@ -9,72 +9,88 @@ namespace MSOEWorkshop
 {
 	public partial class MSOEWorkshopPage : ContentPage
 	{
-		private Emotion currentEmotion;
+		private Emotion[] currentEmotions;
 		private MediaFile picture;
-		public MSOEWorkshopPage ()
+		public MSOEWorkshopPage()
 		{
-			InitializeComponent ();
-			NavigationPage.SetHasNavigationBar (this, false);
-			cameraButton.Clicked += async (sender, e) => {
+			InitializeComponent();
+			NavigationPage.SetHasNavigationBar(this, false);
+			cameraButton.Clicked += async (sender, e) =>
+			{
 				cameraButton.IsEnabled = false;
-				await CrossMedia.Current.Initialize ();
-				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported) {
-					DisplayAlert ("No Camera", "No Camera Available!", "Ok");
+				await CrossMedia.Current.Initialize();
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					DisplayAlert("No Camera", "No Camera Available!", "Ok");
 					return;
 				}
 				this.IsBusy = true;
 				loadingIndicator.IsVisible = true;
 				loadingIndicator.IsRunning = true;
-				picture = await CrossMedia.Current.TakePhotoAsync (new Plugin.Media.Abstractions.StoreCameraMediaOptions {
+				picture = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
 					Directory = "MSOE App",
 					PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
 					DefaultCamera = Plugin.Media.Abstractions.CameraDevice.Front,
-					Name = string.Format ("{0}.jpg", System.DateTime.Now.ToString ("G")),
+					Name = string.Format("{0}.jpg", System.DateTime.Now.ToString("G")),
 				});
-				circleImage.Source = ImageSource.FromStream (() => {
-					var stream = picture.GetStream ();
+				circleImage.Source = ImageSource.FromStream(() =>
+				{
+					var stream = picture.GetStream();
 					return stream;
 				});
-				var emotions = await WebServices.GetHappinessAsync (picture.GetStream ());
-				this.ProcessEmotions (emotions);
+				var emotions = await WebServices.GetHappinessAsync(picture.GetStream());
+				currentEmotions = emotions;
+				this.ProcessEmotions(emotions);
 				this.IsBusy = false;
 				loadingIndicator.IsVisible = false;
 				cameraButton.IsEnabled = true;
 			};
-			circleImage.GestureRecognizers.Add (new TapGestureRecognizer (CircleImageTapped));
+			circleImage.GestureRecognizers.Add(new TapGestureRecognizer(CircleImageTapped));
 		}
 
-		void CircleImageTapped (View arg1, object arg2)
+		void CircleImageTapped(View arg1, object arg2)
 		{
-			Navigation.PushAsync (new EmotionDetailPage (currentEmotion, picture));
+			Navigation.PushAsync(new EmotionDetailPage(currentEmotions, picture));
 		}
 
-		private void ProcessEmotions (Emotion [] emotionArray)
+		private void ProcessEmotions(Emotion[] emotionArray)
 		{
-			if (emotionArray.Length == 0) {
-				DisplayAlert ("Error", "Could not detect any faces", "Ok");
+			if (emotionArray.Length == 0)
+			{
+				DisplayAlert("Error", "Could not detect any faces", "Ok");
 				return;
 			}
-			if (emotionArray.Length == 1) {
-				happinessLabel.Text = string.Format ("1 Person, Happiness: {0} {1}", emotionArray.First ().Scores.Happiness.ToString ("P1"), this.GetSmiley (emotionArray.First ().Scores.Happiness));
+			if (emotionArray.Length == 1)
+			{
+				happinessLabel.Text = string.Format("1 Person, Happiness: {0} {1}", emotionArray.First().Scores.Happiness.ToString("P1"), this.GetSmiley(emotionArray.First().Scores.Happiness));
 				return;
 			}
 			var averageHappiness = 0.0;
-			foreach (var emotion in emotionArray) {
+			foreach (var emotion in emotionArray)
+			{
 				averageHappiness += emotion.Scores.Happiness;
 			}
-			happinessLabel.Text = string.Format ("{0} People, Average Happiness: {1} {2}", emotionArray.Length, averageHappiness.ToString ("P1"), this.GetSmiley (averageHappiness));
+			averageHappiness = averageHappiness / emotionArray.Length;
+			happinessLabel.Text = string.Format("{0} People, Average Happiness: {1} {2}", emotionArray.Length, averageHappiness.ToString("P1"), this.GetSmiley(averageHappiness));
 		}
 
-		private string GetSmiley (double happiness)
+		private string GetSmiley(double happiness)
 		{
-			if (happiness >= 0.75) {
+			if (happiness >= 0.75)
+			{
 				return "\ud83d\ude00"; //smiley with teeth
-			} else if (happiness < 0.75 && happiness >= 0.5) {
+			}
+			else if (happiness < 0.75 && happiness >= 0.5)
+			{
 				return "\ud83d\ude42"; //normal smiley
-			} else if (happiness < 0.5 && happiness >= 0.25) {
+			}
+			else if (happiness < 0.5 && happiness >= 0.25)
+			{
 				return "☹️"; // sad face
-			} else {
+			}
+			else
+			{
 				return "\ud83d\ude2d"; //crying face
 			}
 		}
